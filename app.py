6ba18b787696303501ed3156b8809ab8c7d18e50
petraborus <pborus@islstudent.ch>
2024-07-13 13:26:55 +0200

    update

--- app.py
+++ app.py
@@ -50,6 +50,35 @@ def home():
         return render_template('home.html', tasks=tasks)
 
 
+# delete an item from to do list
+@app.route("/delete/<int:id>")
+def delete(id:int):
+    delete_task = MyTask.query.get_or_404(id)
+    try:
+        db.session.delete(delete_task)
+        db.session.commit()
+        return redirect("/")
+    except Exception as e:
+        return f"ERROR:{e}"
+
+
+# edit an item in to do list
+@app.route("/edit/<int:id>", methods=["GET", "POST"])   # route needs to match in html file (in home.html)
+def edit(id:int):
+    # creating a task
+    task = MyTask.query.get_or_404(id)
+    if request.method == "POST":
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            return f"ERROR:{e}"
+    else:
+        return render_template('edit.html', task=task)
+
+
+
 @app.route("/about")
 def about():
     return render_template('about.html', title="About")
